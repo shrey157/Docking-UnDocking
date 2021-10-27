012750b2e1794704e6aa0c175b7aacf32180c331
shrey157 <76216409+shrey157@users.noreply.github.com>
2021-10-27 22:24:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var iss, spacecraft,satelite;
+var bg, issimg,Sateliteimg, scimg;
+var hasDocked = false;
+
+function preload(){
+  bg= loadImage("space.jpg");
+  issimg = loadImage("iss.png");
+  scimg = loadImage("spacecraft1.png");
+  scimg1 = loadImage("spacecraft2.png");
+  scimg2= loadImage("spacecraft3.png");
+  scimg3= loadImage("spacecraft4.png");
+  Sateliteimg=loadImage('spacecraft.png')
+}
+
+function setup() {
+  createCanvas(600, 350);
+  spacecraft = createSprite(285,240);
+  spacecraft.addImage(scimg);
+  spacecraft.scale = 0.15;
+
+  satelite=createSprite(30,26)
+  satelite.addImage(Sateliteimg)
+  satelite.scale=0.1
+  
+  iss = createSprite(330,130);
+  iss.addImage(issimg);
+  iss.scale = 0.25;
+}
+
+function draw() {
+  background(bg);
+  spacecraft.addImage(scimg);
+  if(!hasDocked){
+
+    // spacecraft = spacecraftx + random(-1,1);
+    // spacecraft.x = spacecraft.x + Random(-1,1);
+    // spacecraft.x = spacecraft.x + randomNumbers(-1,1);
+     spacecraft.x = spacecraft.x + random(-1,1);
+    
+  if(keyDown("UP_ARROW")){
+    spacecraft.y = spacecraft.y -2;
+  }
+
+  if(keyDown("DOWN_ARROW")){
+    spacecraft.y = spacecraft.y +2;
+  }
+    
+  if(keyDown("LEFT_ARROW")){
+    spacecraft.addImage(scimg3);
+    spacecraft.x = spacecraft.x - 1;
+  }
+    
+  if(keyDown("RIGHT_ARROW")){
+    spacecraft.addImage(scimg2);
+    spacecraft.x = spacecraft.x + 1;
+  }
+    
+  if(keyDown("DOWN_ARROW")){
+      spacecraft.addImage(scimg1);
+  }
+}
+
+   if(spacecraft.y <= 199 && spacecraft.x <= (iss.x-10)){
+     hasDocked = true;
+     textSize(25);
+     fill("white")
+     text("Docking Successful!", 200, 300);
+   }
+
+  // if(spacecraft.y <= (iss.y-70) && spacecraft.x <= (iss.x-10)){
+  //   hasDocked = true;
+  //   textSize(25);
+  //   fill("white")
+  //   text("Docking Successful!", 200, 300);
+  // }
+
+
+  // if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
+  //   hasDocked = true;
+  //   textSize(25);
+  //   fill("white")
+  //   text("Docking Successful!", 200, 300);
+  // }
+
+  drawSprites();
+}
+
